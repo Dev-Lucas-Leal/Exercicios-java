7b392499c64b17b5b99d0844f9d091a789045e53
Lucas <llucaslleal2006@gmail.com>
2026-07-08 20:16:22 -0300

    Pastas ExerciciosMatrizes e ExerciciosMetodos criadas.

---
+++ ExercíciosMetodos.java
@@ -0,0 +1,15 @@
+package org.yourcompany.yourproject;
+
+//Autor: Lucas Leal Vieira.
+//Docente: Professor Thyago Maia.
+//Disciplina: Programação Orientada a Objetos.
+
+import java.util.Scanner;
+
+public class ExercíciosMetodos {
+   public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+    }
+    
+}
